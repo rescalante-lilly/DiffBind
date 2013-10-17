c59e6ba7747c1ef590afefeeee1c58d2546cab1a
r.stark <r.stark@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-10-17 11:25:02 +0000

    Fix bug in dba.count relating to miscounting chrN_random reads
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DiffBind@81763 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/intervalSet.h
+++ src/intervalSet.h
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <string>
+#include <stdlib.h>
 #include "interval.h"
 #include "intervalTree.h"
 
@@ -10,14 +11,29 @@ namespace bode {
 
 struct chromComp {
   bool operator()(const std::string &lhs,const std::string &rhs) const {
-    int an = 0,bn = 0;
-    const char *lc,*rc;
-    lc = lhs.c_str();
-    rc = rhs.c_str();
-    an = atoi(lc+3);
-    bn = atoi(rc+3);
-    if (an > 0 && bn > 0) {
-      return an < bn;
+    int ln = 0,rn = 0;
+    char lc[128],rc[128];
+    char *ltrailing,*rtrailing;
+    strncpy(lc,lhs.c_str(),128);
+    strncpy(rc,rhs.c_str(),128);
+
+    if (strncmp(lc,"chr",3) == 0) {
+      ln = strtol(lc+3,&ltrailing,10);
+    } else {
+      ln = strtol(lc,&ltrailing,10);
+    }
+    if (strncmp(rc,"chr",3) == 0) {
+      rn = strtol(rc+3,&rtrailing,10);
+    } else {
+      rn = strtol(rc,&rtrailing,10);
+    }
+
+    if (ln > 0 && rn > 0) {
+      if (ln == rn) {
+        return strcmp(ltrailing,rtrailing) < 0;
+      } else {
+        return ln < rn;
+      }
     } else {
       return lhs < rhs;
     }
@@ -27,7 +43,7 @@ struct chromComp {
 class IntervalSet {
   public:
     IntervalSet();
-    ~IntervalSet();
+    virtual ~IntervalSet();
     virtual void insert(Interval const *inter,bode::NodeGroup* ng);
     void clear(void);
     int coverage(std::string const &chrom,int point) const;
